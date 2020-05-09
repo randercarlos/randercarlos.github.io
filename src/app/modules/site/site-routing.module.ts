05341e9b79f4315645fe33e246f18ad253946956
Rander Carlos Caetano Freitas <randerccf@gmail.com>
2020-05-09 17:40:16 -0300

    create available rooms component

--- src/app/modules/site/site-routing.module.ts
+++ src/app/modules/site/site-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SiteMainComponent } from './__template/site-main/site-main.component';
 import { HomeComponent } from './home/home.component';
+import { AvailableRoomsComponent } from './available-rooms/available-rooms.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     path: '', component: SiteMainComponent,
       children: [
         { path: '', pathMatch: 'full', component: HomeComponent },
+        { path: 'available-rooms', pathMatch: 'full', component: AvailableRoomsComponent },
       ]
   },
 ];
--- src/app/modules/site/site.module.ts
+++ src/app/modules/site/site.module.ts
@@ -9,6 +9,14 @@ import { SiteLoginFormComponent } from './__template/site-login-form/site-login-
 import { SiteSignupFormComponent } from './__template/site-signup-form/site-signup-form.component';
 import { HomeComponent } from './home/home.component';
 import { SliderComponent } from './home/slider/slider.component';
+import { CheckAvailabilityComponent } from './home/check-availability/check-availability.component';
+import { FeaturedRoomsComponent } from './home/featured-rooms/featured-rooms.component';
+import { YourStayComponent } from './home/your-stay/your-stay.component';
+import { OurNewsEventsComponent } from './home/our-news-events/our-news-events.component';
+import { OurServicesComponent } from './home/our-services/our-services.component';
+import { TestemonialComponent } from './home/testemonial/testemonial.component';
+import { NewsletterSignupComponent } from './home/newsletter-signup/newsletter-signup.component';
+import { AvailableRoomsComponent } from './available-rooms/available-rooms.component';
 
 
 @NgModule({
@@ -20,7 +28,15 @@ import { SliderComponent } from './home/slider/slider.component';
     SiteSignupFormComponent,
     SliderComponent,
     HomeComponent,
-    SliderComponent
+    SliderComponent,
+    CheckAvailabilityComponent,
+    FeaturedRoomsComponent,
+    YourStayComponent,
+    OurNewsEventsComponent,
+    OurServicesComponent,
+    TestemonialComponent,
+    NewsletterSignupComponent,
+    AvailableRoomsComponent
   ],
   imports: [
     CommonModule,
